808c0783c71ed0fa2136e8607bf8f7a3be3d8766
SD67119 <manendra.singh@external.stellantis.com>
2023-12-18 16:05:51 +0530

    change auto to idenetity

--- src/main/java/com/quarkus/rest/application/model/Person.java
+++ src/main/java/com/quarkus/rest/application/model/Person.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Person {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     private String name;
 
